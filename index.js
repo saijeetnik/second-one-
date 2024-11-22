ad59cbbde3746cf0a7db45f1f7e36c4f9dba5bcb
saijeetnik <saijeetnik@gmail.com>
2024-11-22 19:36:07 +0530

    adding the file in repo

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const app=express()
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.json( "hi there");
+});
+
+app.listen(3000,()=>{
+    console.log("server is started on 3000 port")
+});
